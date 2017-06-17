976815f9d9a6bb762aaf37a44d06643a4c9f640a
Antonio Cuni <anto.cuni@gmail.com>
2017-06-17 11:55:04 +0200

    try to build and upload also the sdist

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e -x
 
-PYBIN=/opt/python/
 # Compile wheels
 for PYBIN in /opt/python/cp27-*/bin; do
     "${PYBIN}/pip" install 'cython>=0.25'
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+
+    # create the sdist if it does not exist yet
+    if [[ ! -d /io/dist ]]
+    then
+        (cd /io &&
+        "${PYBIN}/python" setup.py sdist &&
+        mkdir -p wheelhouse &&
+        cp dist/*.tar.gz wheelhouse)
+    fi
 done
 
 # Bundle external shared libraries into the wheels
